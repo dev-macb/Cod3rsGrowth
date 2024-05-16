75785376b242efb1da61ba05ea1295ecf57ea68f
dev-macb <dev.macb@gmail.com>
2024-05-16 08:44:57 -0300

    Refatoração das entidades Personagem e Habilidade

--- Cod3rsGrowth.Domain/Habilidade.cs
+++ Cod3rsGrowth.Domain/Habilidade.cs
@@ -4,11 +4,11 @@
     {
         public int Id { get; }
         public string Nome { get; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public DateTime CriadoEm { get; set; }
         public DateTime AtualizadoEm { get; set; }
 
-        public Habilidade(string nome, string descricao)
+        public Habilidade(string nome, string? descricao)
         {
             Nome = nome;
             Descricao = descricao;
--- Cod3rsGrowth.Domain/Personagem.cs
+++ Cod3rsGrowth.Domain/Personagem.cs
@@ -6,16 +6,15 @@
         public string Nome { get; }
         public int Vida { get; set; }
         public int Energia { get; set; }
-        public float Velocidade { get; set; }
+        public double Velocidade { get; set; }
         public CategoriasEnum Forca { get; set; }
         public CategoriasEnum Inteligencia { get; set; }
-        public List<Habilidade> Habilidades { get; set; }
-        public bool EVilao { get; set; }
-        public DateTime CriadoEm { get; set; }
-        public DateTime AtualizadoEm { get; set; }
+        public List<Habilidade>? Habilidades { get; set; }
+        public bool? EVilao { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public DateTime? AtualizadoEm { get; set; }
 
-
-        public Personagem(string nome, int vida, int energia, float velocidade, CategoriasEnum forca, CategoriasEnum inteligencia, List<Habilidade> habilidades, bool EVilao)
+        public Personagem(string nome, int vida, int energia, double velocidade, CategoriasEnum forca, CategoriasEnum inteligencia)
         {
             Nome = nome;
             Vida = vida;
@@ -23,10 +22,22 @@
             Velocidade = velocidade;
             Forca = forca;
             Inteligencia = inteligencia;
-            Habilidades = habilidades;
-            EVilao = ehVilao;
-            CriadoEm = DateTime.Now;
-            AtualizadoEm = DateTime.Now;
+        }
+
+        public int Atacar()
+        {
+            return (int)Forca * Energia;
+        }
+
+        public int Defender()
+        {
+            return (int)Inteligencia * Energia;
+        }
+
+        public void Descansar()
+        {
+            Energia += 10;
+            return;
         }
     }
 }
