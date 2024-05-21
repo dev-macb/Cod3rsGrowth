83c4090d1c904b18213386e4758404f1bb465f09
dev-macb <dev.macb@gmail.com>
2024-05-21 09:42:04 -0300

    Adição de teste de criação de personagem com nome curto

--- Cod3rsGrowth.Tests/Tests/test_personagem_servico.cs
+++ Cod3rsGrowth.Tests/Tests/test_personagem_servico.cs
@@ -21,13 +21,30 @@ namespace Cod3rsGrowth.Tests.Tests
             var novoPersonagem = new Personagem(null, "Teste da Silva", 100, 50, 1.0f, CategoriasEnum.Bom, CategoriasEnum.Medio);
 
             // Act
-            var idCriado = personagemService.Criar(novoPersonagem);
-            var personagemCriado = personagemService.ObterPorId(idCriado);
+            var resultado = personagemService.Criar(novoPersonagem);
+            var personagemCriado = personagemService.ObterPorId(resultado);
 
             // Assert
             Assert.NotNull(personagemCriado);
-            Assert.Equal(personagemCriado.Id, idCriado);
+            Assert.Equal(personagemCriado.Id, resultado);
             Assert.Equal(novoPersonagem.Nome, personagemCriado.Nome);
+        }
+
+        [Fact]
+        public void TestarCriarNovoPersonagemInvalido()
+        {
+            // Arrange
+            Exception? mensagemErro = null;
+            var personagemInvalido = new Personagem(null, "", 100, 50, 1.0f, CategoriasEnum.Bom, CategoriasEnum.Medio);
+
+            // Act
+            try { personagemService.Criar(personagemInvalido); }
+            catch (Exception erro) { mensagemErro = erro; }
+            
+            // Assert
+            Assert.NotNull(mensagemErro);
+            Assert.IsType<Exception>(mensagemErro);
+            Assert.Equal("Falha na validação do personagem", mensagemErro.Message);
         }
     }
 }
