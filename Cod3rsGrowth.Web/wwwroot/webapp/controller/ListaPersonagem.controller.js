8690b72d4e0d1ae641b9ac692a30f6c094f54d56
dev-macb <dev.macb@gmail.com>
2024-07-15 14:05:02 -0300

    .

--- Cod3rsGrowth.Web/wwwroot/webapp/controller/ListaPersonagem.controller.js
+++ Cod3rsGrowth.Web/wwwroot/webapp/controller/ListaPersonagem.controller.js
@@ -7,28 +7,45 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("coders-growth.controller.ListaPersonagem", {
-		onInit: function() {
-            const urlObterTodosPersonagens = "https://localhost:5051/api/Personagem";
-			
-			fetch(urlObterTodosPersonagens, {
-				method: "GET",
-				headers: { "Content-Type": "application/json" },
-			})
-			.then(resposta => {
-				if (resposta.ok) {
-					return resposta.json();
-				} 
-				else {
-					throw new Error('Erro na resposta da API');
-				}
-			})
-			.then(personagens => {
-				const modeloPersonagem = new JSONModel(personagens);
-            	this.getView().setModel(modeloPersonagem);
-			})
-			.catch(erro => {
-				console.error(erro);
+		onInit() {
+			this._personagemModelo = new JSONModel({
+				itens: [],
+				paginaAtual: 1,
+				itensPorPagina: 10,
+				totalPaginas: 0
 			});
+			this.getView().setModel(this._personagemModelo, "paginacao");
+			this._carregarDados();
+        },
+
+		_carregarDados() {
+			try {
+                const urlObterTodosPersonagens = "https://localhost:5051/api/Personagem";
+
+                const resposta = await fetch(urlObterTodosPersonagens, {
+                    method: "GET",
+                    headers: { "Content-Type": "application/json" },
+                });
+
+                if (!resposta.ok) {
+                    throw new Error('Erro na resposta da API');
+                }
+
+                const personagens = await resposta.json();
+                this._oModel.setProperty("/items", personagens);
+                this._updatePaginator();
+                this._updateList();
+            } catch (error) {
+                console.error("Erro ao carregar dados:", error);
+            }
+		},
+
+		_atualizarPaginacao() {
+            const totalItems = this._personagemModelo.getProperty("/itens").length;
+            const itemsPerPage = this._personagemModelo.getProperty("/itensPorPagina");
+            const pageCount = Math.ceil(totalItems / itemsPerPage);
+
+            this._personagemModelo.setProperty("/totalPaginas", pageCount);
         },
 
 		aoFiltrarPersonagemPorNome(evento) {
@@ -41,6 +58,17 @@ sap.ui.define([
 			const oList = this.byId("listaPersonagem");
 			const oBinding = oList.getBinding("items");
 			oBinding.filter(filtro);
+		},
+
+        async aoAbrirFiltros() {
+            this.dialogoFiltros ??= await this.loadFragment({
+                name: "coders-growth.view.ListaPersonagem"
+            });
+            this.dialogoFiltros.open();
+        },
+
+		aoAplicarFiltros() {
+			this.byId("helloDialog").close();
 		}
 	});
 });
